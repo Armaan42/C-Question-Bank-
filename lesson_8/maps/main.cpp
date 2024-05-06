87755a3e22420e1e80d0354e1195838f54674dc0
Armaan <armaansinghbhau8@gmail.com>
2024-05-06 11:24:23 +0530

    lesson_8

---
+++ lesson_8/maps/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+    //creation of map
+    map<int, char> m;
+    m[0] = 'a';
+    m[1] = 'b';
+    m[2] = 'c';
+
+    cout << m[1] << endl;
+
+}
